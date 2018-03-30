cc25ee2d68252df3a647083ee32ef37661f9304b
ipatov <ipatov@jedi-pro.ru>
2018-03-30 22:40:33 +0300

    fix headers

--- SendMailSmtpClass.php
+++ SendMailSmtpClass.php
@@ -12,7 +12,7 @@
 * - Работа с кодировками utf-8 и windows-1251
 * - Возможность отправки нескольким получателям
 * - Автоматическое формирование заголовков письма
-* - Возможность фложения файлов в письмо
+* - Возможность вложения файлов в письмо
 * 
 * @author Ipatov Evgeniy <admin@vk-book.ru>
 * @version 1.1
@@ -64,7 +64,7 @@ class SendMailSmtpClass {
     */
     function send($mailTo, $subject, $message, $smtp_from) {		
 		// подготовка содержимого письма к отправке
-		$contentMail = $this->getContentMail($subject, $message, $smtp_from);		
+		$contentMail = $this->getContentMail($subject, $message, $smtp_from, $mailTo);		
         
         try {
             if(!$socket = @fsockopen($this->smtp_host, $this->smtp_port, $errorNumber, $errorDescription, 30)){
@@ -149,7 +149,7 @@ class SendMailSmtpClass {
 		$data = fread($file,  filesize( $path ) );
 		fclose($file);
 		$filename = basename($path);		
-		$multipart .=  "\r\n--{$this->boundary}\r\n";   
+		$multipart  =  "\r\n--{$this->boundary}\r\n";   
 		$multipart .= "Content-Type: application/octet-stream; name=\"$filename\"\r\n";   
 		$multipart .= "Content-Transfer-Encoding: base64\r\n";   
 		$multipart .= "Content-Disposition: attachment; filename=\"$filename\"\r\n";   
@@ -174,7 +174,7 @@ class SendMailSmtpClass {
     }
 	
 	// подготовка содержимого письма
-	private function getContentMail($subject, $message, $smtp_from){	
+	private function getContentMail($subject, $message, $smtp_from, $mailTo){	
 		// если кодировка windows-1251, то перекодируем тему
 		if( strtolower($this->smtp_charset) == "windows-1251" ){
 			$subject = iconv('utf-8', 'windows-1251', $subject);
@@ -192,6 +192,7 @@ class SendMailSmtpClass {
 			$headers .= "Content-type: text/html; charset={$this->smtp_charset}\r\n"; 			
 		}
 		$headers .= "From: {$smtp_from[0]} <{$smtp_from[1]}>\r\n"; // от кого письмо
+		$headers.= "To: ".$mailTo."\r\n"; // кому
         $contentMail .= $headers . "\r\n";
 		
 		if($this->addFile){
